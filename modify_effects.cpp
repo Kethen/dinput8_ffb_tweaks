aea0147c17200439fbab127363c678637efe0e79
Katharine Chui <katharine.chui@gmail.com>
2024-07-10 16:01:08 +0200

    add type specific effects logging

--- modify_effects.cpp
+++ modify_effects.cpp
@@ -72,7 +72,8 @@ static void modify_effect(LPGUID effect_guid, LPDIEFFECT params, DWORD *modified
 			"attack level: %d\n"
 			"attack time: %d\n"
 			"fade level: %d\n"
-			"fade time: %d\n",
+			"fade time: %d\n"
+			"end envelope",
 			params->lpEnvelope->dwAttackLevel,
 			params->lpEnvelope->dwAttackTime,
 			params->lpEnvelope->dwFadeLevel,
@@ -96,7 +97,89 @@ static void modify_effect(LPGUID effect_guid, LPDIEFFECT params, DWORD *modified
 			LOG_IF_LOG_EFFECTS("trigger button: 0x%08x\n", params->dwTriggerButton);
 		}
 	}
-	// TODO type specific params
+
+	if(*modified_items & DIEP_TYPESPECIFICPARAMS && params->lpvTypeSpecificParams != NULL){
+		LOG_IF_LOG_EFFECTS("type specific params:\n");
+		for(int i = 0;i < params->cbTypeSpecificParams;i++){
+			LOG_IF_LOG_EFFECTS("item #%d:\n", i);
+			// condition effects
+			if(
+				*effect_guid == GUID_Spring ||
+				*effect_guid == GUID_Damper ||
+				*effect_guid == GUID_Inertia ||
+				*effect_guid == GUID_Friction
+			){
+				DICONDITION *base_pointer = (DICONDITION *)params->lpvTypeSpecificParams;
+				LOG_IF_LOG_EFFECTS(
+					"offset: %d\n"
+					"positive coefficient: %d\n"
+					"negative coefficient: %d\n"
+					"positive saturation: %d\n"
+					"negative saturation: %d\n"
+					"dead band: %d\n",
+					base_pointer[i].lOffset,
+					base_pointer[i].lPositiveCoefficient,
+					base_pointer[i].lNegativeCoefficient,
+					base_pointer[i].dwPositiveSaturation,
+					base_pointer[i].dwNegativeSaturation,
+					base_pointer[i].lDeadBand
+				)
+			}
+
+			// periodic effects
+			if(
+				*effect_guid == GUID_Square ||
+				*effect_guid == GUID_Sine ||
+				*effect_guid == GUID_Triangle ||
+				*effect_guid == GUID_SawtoothUp ||
+				*effect_guid == GUID_SawtoothDown
+			){
+				DIPERIODIC *base_pointer = (DIPERIODIC *)params->lpvTypeSpecificParams;
+				LOG_IF_LOG_EFFECTS(
+					"magnitude: %d\n"
+					"offset: %d\n"
+					"phase: %d\n"
+					"period: %d\n",
+					base_pointer[i].dwMagnitude,
+					base_pointer[i].lOffset,
+					base_pointer[i].dwPhase,
+					base_pointer[i].dwPeriod
+				);
+			}
+
+			if(*effect_guid == GUID_CustomForce){
+				DICUSTOMFORCE *base_pointer = (DICUSTOMFORCE *)params->lpvTypeSpecificParams;
+				LOG_IF_LOG_EFFECTS(
+					"sample period: %d\n",
+					base_pointer[i].dwSamplePeriod
+				);
+				LOG_IF_LOG_EFFECTS("force:\n")
+				for(int j = 0;j < base_pointer[i].cSamples;j++){
+					LOG_IF_LOG_EFFECTS("sample #%d: ", j)
+					for(int k = 0;k < base_pointer[i].cChannels;k++){
+						LOG_IF_LOG_EFFECTS("%d ", base_pointer[i].rglForceData[j * base_pointer[i].cChannels + k]);
+					}
+					LOG_IF_LOG_EFFECTS("\n");
+				}
+			}
+
+			if(*effect_guid == GUID_ConstantForce){
+				DICONSTANTFORCE *base_pointer = (DICONSTANTFORCE *)params->lpvTypeSpecificParams;
+				LOG_IF_LOG_EFFECTS("magnitude: %d\n", base_pointer[i].lMagnitude);
+			}
+
+			if(*effect_guid == GUID_RampForce){
+				DIRAMPFORCE *base_pointer = (DIRAMPFORCE *)params->lpvTypeSpecificParams;
+				LOG_IF_LOG_EFFECTS(
+					"start: %d\n"
+					"end: %d\n",
+					base_pointer[i].lStart,
+					base_pointer[i].lEnd
+				);
+			}
+		}
+
+	}
 }
 
 static void __attribute__((stdcall))set_param_cb(LPGUID effect_guid, LPDIEFFECT params, DWORD *modified_items){
