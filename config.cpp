2b8a23ea6cb8a65f4a56388c18bc868782c15a6c
Katharine Chui <katharine.chui@gmail.com>
2024-08-12 13:40:07 +0200

    set file mode when creating files

--- config.cpp
+++ config.cpp
@@ -141,7 +141,7 @@ void parse_config(bool update_if_needed){
 	if(!updated || !update_if_needed){
 		return;
 	}
-	config_fd = open(config_path, O_BINARY | O_WRONLY | O_TRUNC | O_CREAT);
+	config_fd = open(config_path, O_BINARY | O_WRONLY | O_TRUNC | O_CREAT, 00644);
 
 	if(config_fd < 0){
 		LOG("warning: failed opening %s for updating, please make sure the file is not readonly\n", config_path);
